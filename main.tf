2ce1ad7b64f0c45f9ef9984cc4eb361ff5f970f4
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-06-16 12:31:32 +0100

    ignore .gitkeep in fileset

--- main.tf
+++ main.tf
@@ -12,10 +12,11 @@ resource "aws_s3_bucket_object" "bootstrap_dirs" {
 }
 
 resource "aws_s3_bucket_object" "bootstrap_files" {
-  for_each = fileset("${path.root}/${var.local_directory}", "**")
-
-  bucket = aws_s3_bucket.bucket.id
-  key    = each.value
-  source = "${path.root}/${var.local_directory}/${each.value}"
-  etag   = filemd5("${path.root}/${var.local_directory}/${each.value}")
+  # for_each = fileset("${path.root}/${var.local_directory}", "**")
+  # transform the fileset to remove .gitkeep files before uploading files to s3
+  for_each = toset([for file in fileset("${path.root}/${var.local_directory}", "{config,content,license,plugin,software}/**") : replace(file, ".gitkeep", "")])
+  bucket   = aws_s3_bucket.bucket.id
+  key      = each.value
+  source   = "${path.root}/${var.local_directory}/${each.value}"
+  etag     = filemd5("${path.root}/${var.local_directory}/${each.value}")
 }
